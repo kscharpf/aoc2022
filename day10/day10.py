0b8a3c0018ec1e2438fa104cbbbb78221e3daf21
Kevin Scharpf <kevinscharpf@gmail.com>
2022-12-10 11:29:42 -0500

    add documentation

--- day10/day10.py
+++ day10/day10.py
@@ -6,24 +6,36 @@ from typing import List, Union
 
 
 class NoopInstruction:
+    """
+    No-op abstraction
+    """
     def __init__(self) -> None:
+        """
+        constructor for NoopInstruction
+        """
         self.cycles_remaining = 1
         self.value = 0
 
-    def __repr__(self) -> str:
-        return f"NOOP - Remaining {self.cycles_remaining}"
-
 
 class AddxInstruction:
+    """
+    addx instruction abstraction
+    """
     def __init__(self, value: int) -> None:
+        """
+        constructor for AddxInstruction
+        """
         self.cycles_remaining = 2
         self.value = value
 
-    def __repr__(self) -> None:
-        return f"ADDX({self.value}) Remaining {self.cycles_remaining}"
-
-
 def build_instruction(insstr: str) -> Union[NoopInstruction, AddxInstruction]:
+    """
+    Create instruction object from line string
+    Params:
+        insstr: string of form <instruction> Optional[value]
+    Returns:
+        Instruction object
+    """
     fields = insstr.split(" ")
 
     if fields[0] == "addx":
@@ -33,11 +45,27 @@ def build_instruction(insstr: str) -> Union[NoopInstruction, AddxInstruction]:
 
 
 class StateMachine:
+    """
+    CPU abstraction
+    """
     def __init__(self, program: List[Union[NoopInstruction, AddxInstruction]]) -> None:
+        """
+        Constructor for the state machine
+        Params:
+            program: list of instructions
+        Return: None
+        """
         self.program = program
         self.crt: List[str] = []
 
     def draw_pixel(self, cycle: int, sprite_x: int) -> None:
+        """
+        Draw the pixel on the CRT
+        Params:
+            cycle: int - clock cycle
+            sprite_x: int - current sprite position
+        Return: None
+        """
         row_num = (cycle - 1) // 40
         if row_num >= len(self.crt):
             self.crt.append("")
@@ -48,9 +76,21 @@ class StateMachine:
             self.crt[-1] += "."
 
     def display(self) -> str:
+        """
+        Return the CRT as a single string
+        Params: None
+        Return: None
+        """
         return "\n".join(self.crt)
 
     def run(self, cycles_of_interest: List[int]) -> int:
+        """
+        Execute the state machine
+        Params:
+            cycles_of_interest: The cycles to track for the part1 solution
+        Return:
+            Part1 total value
+        """
         active_instruction = self.program[0]
         self.program = self.program[1:]
         total = 0
@@ -75,6 +115,12 @@ class StateMachine:
 
 
 def main(fname: str) -> None:
+    """
+    Execute the day10 solution for parts1 and 2
+    Params:
+        fname: input filename
+    Return: None
+    """
     with open(fname, "r", encoding="utf-8") as infile:
         lines = [line.rstrip() for line in infile.readlines()]
         program = [build_instruction(line) for line in lines]
