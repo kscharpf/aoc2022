2ded92db5bd962c55e417c4573d0fd2e8bc96705
Kevin Scharpf <kevinscharpf@gmail.com>
2022-12-11 18:33:50 -0500

    cleanup

--- day11/day11.py
+++ day11/day11.py
@@ -1,46 +1,59 @@
+"""
+Advent of Code 2022 Day 11 Python solution
+"""
 import argparse as ap
 from dataclasses import dataclass
-from typing import List, Tuple, Dict, Set
+from typing import List, Tuple, Dict
 import re
-import math
 
-
-@dataclass
-class Expression:
-    eval_str: str
+Expression = str
 
 
 def parse_worry_operation(lines: List[str]) -> Expression:
-    print(f"Parse worry operation: {lines[0]}")
+    """
+    Pull out the next worry operation
+    Params: lines - remaining unparsed program lines
+    Returns: None
+    """
     worry_re = re.compile(r".*Operation: new = (.*)$")
     match = worry_re.findall(lines[0])
     assert match
-    print(f"Match {match}")
-    expr_str = match[0]
-    return Expression(expr_str)
+    return Expression(match[0])
 
 
 @dataclass
 class TestOperation:
+    """
+    Class modeling the monkey's test operation
+    """
+
     divisor: int
     throw_true_monkey: int
     throw_false_monkey: int
 
-    def divisible(self, worry: int) -> bool:
-        return worry % self.divisor == 0
-
-    def execute(self, worry: int, all_monkeys: Dict[int, "MonkeyHolding"]) -> None:
-        if self.divisible(worry):
-            # print(f"Test of worry {worry} true - throwing to {self.throw_true_monkey} divisor {self.divisor}")
-            all_monkeys[self.throw_true_monkey].add_item(worry)
+    def execute(self, worry: int, all_monkeys: Dict[int, "Monkey"]) -> None:
+        """
+        Monkey tests the item by determining if the level of worry is divisible
+        by this monkey's divisor. Depending on that result, throw to the next
+        appropriate monkey.
+        Params:
+            worry: level of worry associated with this item
+            all_monkeys: Dictionary mapping monkey ids to monkey objects
+        Returns: None
+        """
+        if worry % self.divisor == 0:
+            all_monkeys[self.throw_true_monkey].item_worries.append(worry)
         else:
-            # print(f"Test of worry {worry} false - throwing to {self.throw_false_monkey} divisor {self.divisor}")
-            all_monkeys[self.throw_false_monkey].add_item(worry)
+            all_monkeys[self.throw_false_monkey].item_worries.append(worry)
 
 
 def parse_test_operation(lines: List[str]) -> TestOperation:
+    """
+    Convert the next three lines into a TestOperation object
+    Params: lines - remaining program lines
+    Returns: TestOperation
+    """
     test_divisor_re = re.compile(r".*Test: divisible by (\d+)$")
-    print(f"parse_test_operation for line {lines[0]}")
     match = test_divisor_re.findall(lines[0])
     assert match
     divisor = int(match[0])
@@ -58,6 +71,11 @@ def parse_test_operation(lines: List[str]) -> TestOperation:
 
 
 def parse_starting_items(lines: List[str]) -> List[int]:
+    """
+    Parse the next line into the list of starting items for the monkey
+    Params: lines - remaining lines
+    Returns: None
+    """
     starting_items_re = re.compile(r"Starting items: (.*)$")
     match = starting_items_re.findall(lines[0])
     assert match
@@ -65,38 +83,69 @@ def parse_starting_items(lines: List[str]) -> List[int]:
     return items
 
 
-class MonkeyHolding:
+class Monkey:
+    """
+    Monkey abstraction
+    """
+
     def __init__(
         self,
         item_worries: List[int],
         expression: Expression,
         test_operation: TestOperation,
     ) -> None:
+        """
+        Constructor for the monkey object
+        Params:
+            item_worries: initial worry level for each item
+            expression: expression that details how the worry level
+                        for an item changes on inspection
+            test_operation: The test operation this monkey applies
+        """
         self.item_worries = item_worries
         self.expression = expression
         self.test_operation = test_operation
         self.total_inspections = 0
 
-    def add_item(self, item: int) -> None:
-        self.item_worries.append(item)
-
     def inspect(
-        self, all_monkeys: Dict[int, "MonkeyHolding"], worry_drop: int, modulus: int,
+        self,
+        all_monkeys: Dict[int, "Monkey"],
+        worry_drop: int,
+        modulus: int,
     ) -> None:
+        """
+        This monkey inspects each of the items in its possession in order of
+        receipt. To avoid growing integers without bounds, after calculating
+        the new value, the modulus is applied to reduce the value without
+        changing the mathematics.
+        Params:
+            all_monkeys: Dictionary mapping a monkey index to a monkey
+            worry_drop: The mount of decrease in the worry after inspection
+            modulus: Product of all monkey test operation divisors
+        Returns: None
+        """
         for old in self.item_worries:  # ignore: unused-variable
-            new_val = eval(self.expression.eval_str)
+            new_val = eval(self.expression)
             new_val //= worry_drop
             new_val %= modulus
-            # print(f"Inspecting item {old} new value {new_val}")
             self.test_operation.execute(new_val, all_monkeys)
             self.total_inspections += 1
         self.item_worries = []
 
     def __repr__(self) -> str:
+        """
+        Return a string representation of the monkey
+        """
         return ",".join([str(item) for item in self.item_worries])
 
 
-def parse_monkey(lines: List[str]) -> Tuple[int, MonkeyHolding]:
+def parse_monkey(lines: List[str]) -> Tuple[int, Monkey]:
+    """
+    Parse all of the data associated with a given monkey
+    Params:
+        lines: All lines in the program
+    Returns: tuple of monkey id, monkey
+    """
     monkey_id_re = re.compile(r"Monkey (\d+):")
     match = monkey_id_re.findall(lines[0])
     assert match
@@ -105,14 +154,22 @@ def parse_monkey(lines: List[str]) -> Tuple[int, MonkeyHolding]:
     starting_items = parse_starting_items(lines[1:])
     worry_operation = parse_worry_operation(lines[2:])
     test_operation = parse_test_operation(lines[3:])
-    return monkey_id, MonkeyHolding(starting_items, worry_operation, test_operation)
+    return monkey_id, Monkey(starting_items, worry_operation, test_operation)
 
 
 def main(fname: str, worry_drop: int, num_rounds: int) -> None:
+    """
+    Main function for Day11
+    Params:
+        fname: python
+        worry_drop: Divisor indicating the drop in worry over monkey inspection
+        num_rounds: The number of rounds to monitor
+    Returns: None
+    """
     with open(fname, "r", encoding="utf-8") as infile:
         lines = [line.rstrip("\n") for line in infile.readlines()]
         i = 0
-        monkey_holdings: Dict[int, MonkeyHolding] = {}
+        monkey_holdings: Dict[int, Monkey] = {}
         test_prod = 1
         while i < len(lines):
             if lines[i]:
@@ -126,18 +183,17 @@ def main(fname: str, worry_drop: int, num_rounds: int) -> None:
         monkeys = sorted(list(monkey_holdings.keys()))
         for _inspection_round in range(1, num_rounds + 1):
             for monkey_id in monkeys:
-                monkey_holdings[monkey_id].inspect(monkey_holdings, worry_drop, test_prod)
-            # print("------------------------------------")
-            # print(f"Results after round {inspection_round}")
-            # print("------------------------------------")
-            # for monkey_id in monkeys:
-            # print(f"Monkey {monkey_id}: {monkey_holdings[monkey_id]}")
+                monkey_holdings[monkey_id].inspect(
+                    monkey_holdings, worry_drop, test_prod
+                )
         inspections = sorted(
             [monkey_holdings[monkey_id].total_inspections for monkey_id in monkeys],
             reverse=True,
         )
         print(
-            f"Most active {inspections[0]} second {inspections[1]} Part 1 Result {inspections[0]*inspections[1]}"
+            f"Most active {inspections[0]} "
+            f"second {inspections[1]} "
+            f"Part 1 Result {inspections[0]*inspections[1]}"
         )
 
 
